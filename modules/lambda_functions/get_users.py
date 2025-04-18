0f56594a25f23eb28dfdfa123379f75910ae3e3b
Ubuntu <ubuntu@ip-172-31-95-39.ec2.internal>
2025-04-18 12:32:55 +0000

    my commit

--- modules/lambda_functions/get_users.py
+++
@@ -1,8 +0,0 @@
-import boto3
-
-dynamodb = boto3.resource('dynamodb')
-table = dynamodb.Table('serverless_workshop_intro')
-
-def lambda_handler(event, context):
-    data = table.scan()
-    return data['Items']
