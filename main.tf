2761d1d2e6bd4779bab2710fbc635768d72872a3
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-19 16:02:42 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,6 +11,8 @@ module "lambda" {
   source              = "./modules/lambda"
   lambda_function_name = var.lambda_function_name
   dynamodb_table_name = var.dynamodb_table_name
+add_user_zip_file       = "modules/lambda_functions/add_user.zip"
+  get_users_zip_file       = "modules/lambda_functions/get_users.zip"
   lambda_exec_role_arn = module.iam.lambda_exec_role_arn
 }
 
