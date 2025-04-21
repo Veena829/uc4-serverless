16078b01eb1bd3ecbacaa1c2050201c497238e07
Veena829 <78190932+Veena829@users.noreply.github.com>
2025-04-21 11:54:42 +0530

    Update variables.tf

--- modules/lambda/variables.tf
+++ modules/lambda/variables.tf
@@ -12,3 +12,13 @@ variable "lambda_exec_role_arn" {
   description = "IAM role ARN for Lambda execution"
   type        = string
 }
+ variable "add_user_zip_file" {
+   description = "Path to the add_user.zip file"
+   type        = string
+ }
+
+ variable "get_users_zip_file" {
+   description = "Path to the get_users.zip file"
+   type        = string
+ }
+ ```
